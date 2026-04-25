d8bb66d396d0c079922abf6c538e87b3961130f5
Kapil Arora <kaparora@gmail.com>
2026-04-25 17:50:40 +0200

    Add construction banner to notify visitors site is under development

---
+++ app/components/ConstructionBanner.tsx
@@ -0,0 +1,9 @@
+export default function ConstructionBanner() {
+  return (
+    <div className="bg-yellow-100 border-b-4 border-myo-yellow px-4 py-3 text-center">
+      <p className="text-sm font-semibold text-gray-800">
+        🚧 <span className="text-myo-blue">This website is under construction</span> - We're adding more content soon!
+      </p>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ConstructionBanner from "./components/ConstructionBanner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
+        <ConstructionBanner />
         <main className="flex-1">{children}</main>
         <Footer />
       </body>
